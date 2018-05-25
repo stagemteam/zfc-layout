93eca675b1474b77018328f8ac01c96bfd39c6e5
popovsergiy <popow.sergiy@gmail.com>
2018-05-25 17:55:05 +0300

    Module has updated for zf-skeleton

--- config/module.config.php
+++ config/module.config.php
@@ -2,14 +2,8 @@
 namespace Stagem\ZfcLayout;
 
 return [
-
-    // \Zend\Stdlib\ArrayUtils::merge()
-    'assetic_configuration' => require_once 'assets.config.php',
-
-    // middleware
-    //'templates' => [
-    //    'paths' => [
-    //        'widget' => [__DIR__ . '/../view/widget'],
-    //    ],
-    //],
+    'assetic_configuration' => array_merge_recursive(
+        require_once realpath('config/assets.php'),
+        require_once 'assets.config.php'
+    ),
 ];
