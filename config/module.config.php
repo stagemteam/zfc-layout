cc7b4f8da14608be69d0be53f576ba97eb054f84
Andrew Belov <fearess7@gmail.com>
2019-01-29 14:12:30 +0200

    Changed default layout

--- config/module.config.php
+++ config/module.config.php
@@ -39,7 +39,7 @@ return [
         'template_map' => [
             'layout/layout' => __DIR__ . '/../view/layout/layout.phtml',
             'layout::layout' => __DIR__ . '/../view/layout/layout.phtml',
-            //'layout/default'           => __DIR__ . '/../view/layout/layout.phtml',
+            'layout::default' => __DIR__ . '/../view/layout/layout.phtml',
             'error/404' => __DIR__ . '/../view/error/404.phtml',
             'error/index' => __DIR__ . '/../view/error/index.phtml',
         ],
